d17b8265230e6e5f5a2b68e9eda8a3c1b8f6f1e7
holoborodko2800@gmail.com <holoborodko2800@gmail.com>
2024-07-29 19:43:08 +0300

    lab6 i2c added

---
+++ lab6/WindowsFormsApplication1/WindowsFormsApplication1/Form1.cs
@@ -0,0 +1,91 @@
+﻿using System;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Windows.Forms;
+using System.IO.Ports;
+using System.Threading;
+
+namespace WindowsFormsApplication1
+{
+    public partial class Form1 : Form
+    {   
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        
+        private void comboBox1_Click(object sender, EventArgs e)
+        {
+            int num;
+            comboBox1.Items.Clear();
+            string[] ports = SerialPort.GetPortNames().OrderBy(a => a.Length > 3 && int.TryParse(a.Substring(3), out num) ? num : 0).ToArray();
+            comboBox1.Items.AddRange(ports);
+        }
+
+        private void buttonOpenPort_Click(object sender, EventArgs e)
+        {
+            if (!serialPort1.IsOpen)
+                try
+                {
+                    serialPort1.PortName = comboBox1.Text;
+                    serialPort1.BaudRate = 9600;
+                    serialPort1.Open();
+                    buttonOpenPort.Text = "Close";
+                    comboBox1.Enabled = false;
+                    button1.Visible = true;
+                    button2.Visible = true;
+                    button3.Visible = true;
+                    button4.Visible = true;
+                }
+                catch
+                {
+                    MessageBox.Show("Port " + comboBox1.Text + " is invalid!", "Error", 
+                        MessageBoxButtons.OK, MessageBoxIcon.Error);
+                }
+            else
+            {
+                serialPort1.Close();
+                buttonOpenPort.Text = "Open";
+                comboBox1.Enabled = true;
+                button1.Visible = false;
+                button2.Visible = false;
+                button3.Visible = false;
+                button4.Visible = false;
+            }//pavelchak
+        }
+
+        private void writeByteToArduino(byte command)
+        {
+            byte[] b1 = new byte[1];
+            b1[0] = command;
+            serialPort1.Write(b1, 0, 1);
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            writeByteToArduino(0xA1);
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            writeByteToArduino(0xA2);
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            writeByteToArduino(0xB1);
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            writeByteToArduino(0xB2);
+        }
+
+        private void serialPort1_DataReceived(object sender, SerialDataReceivedEventArgs e)
+        {
+            string receivedData = serialPort1.ReadExisting();
+            Invoke(new Action(() => textBox1.AppendText(receivedData)));
+        }
+    }
+}
